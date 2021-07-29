b22dbd4bc7d882e19b63757dead2e851e9c488fe
manjusign <manjusrimj@gmail.com>
2021-07-29 15:20:27 +0530

    new change in dev

--- Demo1.java
+++ Demo1.java
@@ -10,6 +10,7 @@ class Demo1
 		System.out.println("test implemented in sample1 class");
 		System.out.println("after commit next cahnge");
 		System.out.println("after removing next satge");
+		System.out.printl("new change in dev branch");
 	
 	
 }
