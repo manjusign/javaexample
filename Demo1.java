fa2d2e34835bac3e784737f4083fece8e3fdf549
manjusign <manjusrimj@gmail.com>
2021-07-29 14:42:48 +0530

    updated Demo1.java

--- Demo1.java
+++ Demo1.java
@@ -8,6 +8,8 @@ class Demo1
 	public static vois main(String[] args)
 	{
 		System.out.println("test implemented in sample1 class");
-	}
+		System.out.println("after commit next cahnge");
+		System.out.println("after removing next satge");
+	
 	
 }
